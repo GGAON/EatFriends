661cc175fdc4a05b156bb353568f52557125c07f
gaon <kaonsky@gmail.com>
2021-09-14 01:22:02 +0900

    OrderActivity.kt 생성

---
+++ app/src/main/java/com/zeronine/project1/OrderActivity.kt
@@ -0,0 +1,13 @@
+package com.zeronine.project1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class OrderActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
